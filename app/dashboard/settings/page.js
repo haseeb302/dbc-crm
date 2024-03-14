8bf9f293c3bc9f6bf4a635bc92f8ecbeb99c3008
fullstack-developer23 <ahmadsafeer1997gmail.com>
2024-03-14 01:25:16 +0000

    completed settings page UI

--- app/dashboard/settings/page.js
+++ app/dashboard/settings/page.js
@@ -1,3 +1,23 @@
+"use client";
+
+import { Button } from "@/app/ui/components/ui/button";
+import { Input } from "@/app/ui/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+
 export default function Page() {
-  return <h1>Settings</h1>;
+  return (
+    <div className="pl-4 space-y-3 overflow-hidden">
+      <br />
+      <h1 className="text-xl">
+        <b>Settings</b>
+      </h1>
+      <hr />
+      <br />
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="test">Input Box</Label>
+        <Input type="text" id="test" />
+      </div>
+      <Button>Save</Button>
+    </div>
+  );
 }
