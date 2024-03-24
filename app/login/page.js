6423a70d069f3cab04bc6073560ca9d8f1978140
fullstack-developer23 <ahmadsafeer1997gmail.com>
2024-03-24 22:32:39 +0000

    changed title to DBC-Project from DBC

--- app/login/page.js
+++ app/login/page.js
@@ -9,7 +9,7 @@ export default function page() {
     <section class="bg-gray-50 dark:bg-gray-900">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <h1 class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          DBC
+          DBC-Project
         </h1>
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
